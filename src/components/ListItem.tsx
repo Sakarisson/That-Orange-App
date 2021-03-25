e0c10797eed7ca4c9dbbed82dd35bc0fd0b2fde9
Kristian Sakarisson <kristian.sakarisson@wolt.com>
2021-03-25 14:35:08 +0200

    Add possibility to open URL

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 import styled, { useTheme } from 'styled-components';
 import { Body, Subtitle } from '../style/typography/Typography';
 
 const Container = styled(View)`
-  background-color: ${({ theme }) => theme.colors.BACKGROUND};
-`;
-
-const InnerContainer = styled(View)`
   padding: 16px 8px;
   flex-direction: column;
-`;
-
-const Divider = styled(View)`
-  height: 1px;
-  background-color: ${({ theme }) => theme.colors.DIVIDER};
 `;
 
 type Props = {
   label: string;
   sublabel?: string;
+  onPress?: () => void;
 };
 
-const ListItem = ({ label, sublabel }: Props) => {
+const ListItem = ({ label, sublabel, onPress }: Props) => {
   const { colors } = useTheme();
   return (
-    <Container>
-      <InnerContainer>
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => ({ opacity: pressed ? 0.4 : 1 })}>
+      <Container>
         <Body>{label}</Body>
         <Subtitle color={colors.TEXT_SECONDARY}>{sublabel}</Subtitle>
-      </InnerContainer>
-      <Divider />
-    </Container>
+      </Container>
+    </Pressable>
   );
 };
 
--- src/screens/StoryListItem.tsx
+++ src/screens/StoryListItem.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import useSWR from 'swr';
+import { InAppBrowser } from 'react-native-inappbrowser-reborn';
+
 import { getItem } from '../api/hackerNewsApi';
 import ListItem from '../components/ListItem';
 import getHostnameFromUrl from '../util/getHostnameFromUrl';
+import { useTheme } from 'styled-components';
 
 type Props = {
   id: string;
 };
 
 const StoryListItem = ({ id }: Props) => {
+  const theme = useTheme();
+
   const { data } = useSWR(`story-${id}`, () => getItem(id));
 
   if (!data?.result) {
@@ -17,7 +22,18 @@ const StoryListItem = ({ id }: Props) => {
 
   const hostName = getHostnameFromUrl(data.result.url);
 
-  return <ListItem label={data.result.title} sublabel={hostName} />;
+  const onPress = () => {
+    InAppBrowser.open(data.result.url, {
+      animated: true,
+      preferredBarTintColor: theme.colors.BACKGROUND,
+      readerMode: true,
+      modalEnabled: true,
+    });
+  };
+
+  return (
+    <ListItem label={data.result.title} sublabel={hostName} onPress={onPress} />
+  );
 };
 
 export default StoryListItem;
